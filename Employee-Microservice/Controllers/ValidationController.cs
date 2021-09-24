7e735d9f0bd628139eab884f98daf4353666a2f7
saicharan.tammineni <saicharan.tammineni@cgi.com>
2021-09-24 10:11:15 +0530

    New project added

---
+++ Employee-Microservice/Controllers/ValidationController.cs
@@ -0,0 +1,39 @@
+﻿using Employee_Microservice.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_Microservice.Controllers
+{
+    public class ValidationController : Controller
+    {
+
+        CoreDbContext db = new CoreDbContext();
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Validate(Employee admin)
+        {
+            var _admin = db.Employees.Where(s => s.UserName == admin.UserName);
+            if (_admin.Any())
+            {
+                if (_admin.Where(s => s.Passcode == admin.Passcode).Any())
+                {
+
+                    return Json(new { status = true, message = "Login Successfull!" });
+                }
+                else
+                {
+                    return Json(new { status = false, message = "Invalid Password!" });
+                }
+            }
+            else
+            {
+                return Json(new { status = false, message = "Invalid Email!" });
+            }
+        }
+    }
+}
